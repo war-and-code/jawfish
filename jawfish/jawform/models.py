26ff0cc4474ecfc8a9387d5b320892802f4831ee
Randy Gingeleski <rjg26247@gmail.com>
2015-05-15 19:13:58 +0000

    started django integration, made simple data model 'attempt'

---
+++ jawfish/jawform/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+
+class Attempt(models.Model):
+    target = models.CharField(max_length=200)
+    addr = models.CharField(max_length=200)
+    vuln_var = models.CharField(max_length=200)
+    method = models.CharField(max_length=200)
+    goal_text = models.CharField(max_length=200)
+    
