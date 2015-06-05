81158528716f6cbe77a9653526682a9bad89da93
Randy Gingeleski <rgingeleski@aol.com>
2015-06-05 16:32:16 +0200

    implemented submit link post

---
+++ app/targetform.py
@@ -0,0 +1,10 @@
+from flask.ext.wtf import Form
+from wtforms import SelectField, StringField
+from wtforms.validators import DataRequired
+
+class TargetForm(Form):
+    TARGET = StringField('TARGET', validators=[DataRequired()])
+    ADDR = StringField('ADDR', validators=[DataRequired()])
+    VULN_VAR = StringField('VULN_VAR', validators=[DataRequired()])
+    METHOD = SelectField('METHOD', choices=[('GET', 'GET'), ('POST', 'POST')])
+    GOAL_TEXT = StringField('GOAL_TEXT', validators=[DataRequired()])
